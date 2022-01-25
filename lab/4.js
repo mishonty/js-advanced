d5844f2054cbb3c86ac953f8d5a897a455f971dc
Mikhail <80749024+mishonty@users.noreply.github.com>
2022-01-25 20:51:46 +0200

    Update 4.js

--- lab/4.js
+++ lab/4.js
@@ -6,3 +6,14 @@ function editElement(ref, match, replacer) {
     const edited = content.replace(matcher, replacer);
     ref.textContent = edited;
   }
+
+// 2
+function extractText() {
+    
+    const textLiElement = document.querySelectorAll('ul#items li')
+    const textArea = document.querySelector('#result');
+
+    for (const el of textLiElement) {
+        textArea.value += el.textContent + '\n';
+    }
+}
