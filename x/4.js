21671eb65687419ef1887211497b1ed74edd6c68
Mikhail <80749024+mishonty@users.noreply.github.com>
2022-01-26 11:27:10 +0200

    Create 4.js

---
+++ x/4.js
@@ -0,0 +1,11 @@
+// 1
+function subtract() {
+
+    let firstNumElement = document.getElementById('firstNumber').value;
+    let secondNumElement = document.getElementById('secondNumber').value;
+
+    let resultElement = document.getElementById('result');
+    resultElement.textContent = Number(firstNumElement) - Number(secondNumElement);
+}
+
+// 2
